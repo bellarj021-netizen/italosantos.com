42f49d8a230b78f13893b6ac552235cf4e8712d3
Italo Santos <bellarj021@gmail.com>
2025-10-24 21:10:36 -0300

    Add Terraform configuration for Azure infrastructure with Container Registry and App Service

---
+++ .azure/terraform/main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "example" {
+  name                = "exampleregistry"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Basic"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier     = "Basic"
+    size     = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
